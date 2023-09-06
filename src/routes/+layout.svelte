3f7a0ba4e6cf22d35657597103191bc40838bc7f
Aaron N <porkyneal@gmail.com>
2023-09-06 14:50:21 +0100

    Manifest checks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,13 +8,12 @@
     <NavBrand>
         <DarkMode/>
         <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-3">
-      <a href={base}>Svelte kit / Flowbite - starter </a>
+      <a href={base}> </a>
     </span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
     <NavUl {hidden}>
-        <NavLi href="docs" active={$page.url.pathname.endsWith('docs')}>DOCS</NavLi>
-        <NavLi href="https://retech-software.de" >RETECH</NavLi>
+        <NavLi>INSTALL</NavLi>
     </NavUl>
 </Navbar>
 <div class="mt-5 w-4/5 mx-auto">
